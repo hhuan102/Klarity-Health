3dc7e408322d4da99ae9252e22ddc98a85df593c
hhuan102 <hhuan102@ucsc.edu>
2022-10-24 15:57:48 -0700

    1st version

---
+++ database.py
@@ -0,0 +1,31 @@
+# flask_graphene_mongo/database.py
+from mongoengine import connect
+
+from models import Department, Employee, Role
+
+# You can connect to a real mongo server instance by your own.
+connect('graphene-mongo-example', host='mongomock://localhost', alias='default')
+
+
+def init_db():
+    # Create the fixtures
+    engineering = Department(name='Engineering')
+    engineering.save()
+
+    hr = Department(name='Human Resources')
+    hr.save()
+
+    manager = Role(name='manager')
+    manager.save()
+
+    engineer = Role(name='engineer')
+    engineer.save()
+
+    peter = Employee(name='Peter', department=engineering, role=engineer)
+    peter.save()
+
+    roy = Employee(name='Roy', department=engineering, role=engineer)
+    roy.save()
+
+    tracy = Employee(name='Tracy', department=hr, role=manager)
+    tracy.save()
